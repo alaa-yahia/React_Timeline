dbd49185be70c459cfba6b1ebf883b479bf98c2d
Alaa Yahia <alaay873@live.com>
2023-10-22 00:47:12 +0300

    basic timeline

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
